151dce47abe86b9fd7232b9fa2f8c1c9f610c557
vladimirivkovic <vlado.ivkovic@live.com>
2020-07-01 01:21:00 +0200

    minor changes

--- blocks/get_blocks.sh
+++ blocks/get_blocks.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 docker exec -it shop-peer bash -c 'mkdir /home/blocks' 
+N=20
 
-for i in {0..13}
+for i in $(seq 0 $N)
 do
     docker exec -it shop-peer bash -c \
         "peer channel fetch -c default $i /home/blocks/$i.blk"
@@ -9,7 +10,7 @@ done
 
 docker cp shop-peer:/home/blocks .
 
-for i in {0..13}
+for i in $(seq 0 $N)
 do
     ../configtxlator proto_decode --type=common.Block --input=blocks/$i.blk --output=blocks/$i.json
 done
